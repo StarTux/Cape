cb38986fa7e46d2e0952d122f18c9a70c7927572
StarTux <ths.loy@googlemail.com>
2017-07-06 14:30:32 +0200

    Speed up a teensy bit

--- src/main/java/com/winthier/cape/CapePlugin.java
+++ src/main/java/com/winthier/cape/CapePlugin.java
@@ -69,10 +69,9 @@ public final class CapePlugin extends JavaPlugin implements Listener {
             if (!gameModes.contains(player.getGameMode())) return;
             player.setAllowFlight(true);
             player.setFlying(true);
-            player.setFlySpeed(0.025f);
+            player.setFlySpeed(0.0325f);
             Location loc = player.getEyeLocation();
             loc.getWorld().playSound(loc, Sound.BLOCK_END_PORTAL_FRAME_FILL, SoundCategory.PLAYERS, 0.3f, 0.75f);
-            // loc.getWorld().playSound(loc, Sound.BLOCK_ENCHANTMENT_TABLE_USE, SoundCategory.PLAYERS, 0.5f, 2.0f);
             loc.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, loc, 16, 1, 1, 1, 0.0);
         } else {
             if (!flyingPlayers.contains(player)) return;
